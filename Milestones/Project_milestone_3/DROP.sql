45b03852c136ad97ef51c9018e1d54d4afd61738
Gaurang Ruparelia <gr2159@nyu.edu>
2023-11-08 19:19:34 -0500

    add drop

---
+++ Milestones/Project_milestone_3/DROP.sql
@@ -0,0 +1,11 @@
+
+-- drop table if exists in the order shown above
+DROP TABLE appeals IF exists;
+DROP TABLE crime_charges IF exists;
+DROP TABLE sentences IF exists;
+DROP TABLE prob_officer IF exists;
+DROP TABLE crimes IF exists;
+DROP TABLE alias IF exists;
+DROP TABLE criminals IF exists;
+
+
